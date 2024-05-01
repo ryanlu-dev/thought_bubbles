1b17b5307fb121a5542afd4f0f4c4574e5f795ea
singh-maya <87378136+singh-maya@users.noreply.github.com>
2024-05-01 17:18:25 -0400

    brought back everythingggg (#16)
    * added bootstrap to session
    
    * modified replies
    
    * added styling of heart functionality -> can click and unclick and image would adjust
    
    * brought back annie's work
    
    * change case of $studentName so stops throwing error
    
    ---------
    
    Co-authored-by: Ryan Lu <73496995+ryanlu-dev@users.noreply.github.com>
    Co-authored-by: ryanlu-dev <ryan@ryanlu.dev>

--- public/student_view/post_reply.php
+++ public/student_view/post_reply.php
@@ -1,54 +1,54 @@
 <?php
-    // Establish database connection
-    $config = parse_ini_file("../../../database/db_config.ini");
+// Establish database connection
+$config = parse_ini_file("../../../database/db_config.ini");
 
-    $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
+$conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    session_start();
+session_start();
 
-    // Retrieve session code from session variable
-    if (isset($_SESSION['StudentID']) && isset($_SESSION['displayName']) && isset($_SESSION['studentName']) && isset($_SESSION['sessionID'])) {
-        $StudentID = $_SESSION['StudentID'];
-        $displayName = $_SESSION['displayName'];
-        $StudentName = $_SESSION['studentName'];
-        $sessionID = $_SESSION['sessionID'];
-        
-        echo "Student ID : " . $StudentID . "<br>";
-        echo "Session ID : " . $sessionID . "<br>";
-        echo "Session displayName : " . $displayName . "<br>";
-        echo "Session StudentName : " . $StudentName . "<br>";
+// Retrieve session code from session variable
+if (isset($_SESSION['StudentID']) && isset($_SESSION['displayName']) && isset($_SESSION['studentName']) && isset($_SESSION['sessionID'])) {
+    $StudentID = $_SESSION['StudentID'];
+    $displayName = $_SESSION['displayName'];
+    $StudentName = $_SESSION['studentName'];
+    $sessionID = $_SESSION['sessionID'];
 
+    echo "Student ID : " . $StudentID . "<br>";
+    echo "Session ID : " . $sessionID . "<br>";
+    echo "Session displayName : " . $displayName . "<br>";
+    echo "Session StudentName : " . $StudentName . "<br>";
+
+} else {
+    echo "Session code not found.";
+    return;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $interactionType = "reply"; // Fixed interaction type for replying to the response
+    $reply = $_POST['reply-content'];
+    $parentID = $_POST['parentID'];
+
+    // Insert the reply into the interactions table
+    $sql = "INSERT INTO interactions (SessionID, ParentID, StudentID, InteractionType, Content, Timestamp) VALUES (?, ?, ?, ?, ?, DEFAULT)";
+    $stmt = $conn->prepare($sql);
+    if ($stmt) {
+        $stmt->bind_param("iiiss",$sessionID, $parentID, $StudentID, $interactionType, $reply);
+        if ($stmt->execute()) {
+            header('Location: session.php');
+        } else {
+            echo "Error executing query: " . $stmt->error;
+        }
+        $stmt->close();
     } else {
-        echo "Session code not found.";
-        return;
+        echo "Error preparing statement: " . $conn->error;
     }
+}
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    	$interactionType = "reply"; // Fixed interaction type for replying to the response
-    	$reply = $_POST['reply-content'];
-        $parentID = $_POST['parentID'];
-    
-    	// Insert the reply into the interactions table
-    	$sql = "INSERT INTO interactions (SessionID, ParentID, StudentID, InteractionType, Content, Timestamp) VALUES (?, ?, ?, ?, ?, DEFAULT)";
-    	$stmt = $conn->prepare($sql);
-    	if ($stmt) {
-    		$stmt->bind_param("iiiss",$sessionID, $parentID, $StudentID, $interactionType, $reply);
-    		if ($stmt->execute()) {
-    			header('Location: session.php');
-    		} else {
-    			echo "Error executing query: " . $stmt->error;
-    		}
-    		$stmt->close();
-    	} else {
-    		echo "Error preparing statement: " . $conn->error;
-    	}
-    }
-
-    // Close database connection
-    $conn->close();
+// Close database connection
+$conn->close();
 ?>
